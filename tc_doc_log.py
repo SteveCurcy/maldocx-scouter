066a181aaa7c2341a70f9181e3391df1409c96f1
SteveCurcy <steve_curcy@163.com>
2022-10-09 16:07:12 +0800

    update the code and now it can finish a tiny document detection. but some bug there for a large file.

--- tc_doc_log.py
+++ tc_doc_log.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
+# coding = utf-8
+#
+# tc_doc_log.py
+# eBPF application that parses HTTP response packets
+# and extracts (and prints on screen) the document file
+# contained in the Response Stream.
+#
+# eBPF program tc_doc_log.py is used as traffic-controler attached to veth (if it can) interface.
+# Only packets of type ip and tcp containing document are
+# returned to userspace, others passed directly.
+#
+# Python script uses bcc BPF Compiler Collection by
+# iovisor (https://github.com/iovisor/bcc) and prints on stdout the files' information
+# and statistic status.
+#
+# Copyright (c) Steve Curcy
+# October 2022
+# Licensed under the GPL, Version 3.0 (the "License")
 
 from bcc import BPF
 import ctypes as ct
@@ -6,46 +24,118 @@ import pyroute2
 import socket
 import os
 import re
+import pandas as pd
+import joblib
+import argparse
 
-dev = ""
+SIGNATURE_LOCAL_FILE_HEADER = 0x04034b50
+SIGNATURE_CENTRAL_DIRECTORY = 0x02014b50
+
+verbose = False
+dev = None
+is_docx = False
+dt_model = None
+sessions = None
+# file's status
 flags = 0
-file_sz = 0
+cont_sz = 0
 exec_sz = 0
 media_sz = 0
 rest = 0
-is_res = False
+
 
 # find the first vitual eth
-p = os.popen("ip a")
-search_obj = re.search(r'veth[a-z0-9]+', p.read())
-p.close()
-if search_obj:
-    dev = search_obj.group()
-else:
-    print("*** No container is running.")
-    # exit(0)
-    dev = "ens33"
+def auto_get_eth():
+    global dev
+    p = os.popen("ip a")
+    search_obj = re.search(r'veth[a-z0-9]+', p.read())
+    p.close()
+    if search_obj:
+        dev = search_obj.group()
+    else:
+        print("*** No container is running, default interface (ens33) will be used.")
+        # exit(0)
+        dev = "ens33"
 
 
 def print_skb_event(cpu, data, size):
-    global file_sz, exec_sz, media_sz, rest, is_res
+    global cont_sz, exec_sz, media_sz, rest, sessions, is_docx, dt_model, verbose
     raw_sz = size - ct.sizeof(ct.c_uint32)
 
-    class Leaf(ct.Structure):
-        _fields_ = [("file_sz", ct.c_uint32),
-                    ("exec_sz", ct.c_uint32),
-                    ("media_sz", ct.c_uint32),
-                    ("rest", ct.c_uint32)]
-
     class SkbEvent(ct.Structure):
-        _fields_ = [("leaf", Leaf),
-        # _fields_ = [("magic", ct.c_ubyte),
+        _fields_ = [("offset", ct.c_uint32),
+                    ("real_len", ct.c_uint32),
                     ("raw", ct.c_ubyte * raw_sz)]
 
-    # get the Ctype raw data in bytes.
+    # get the Ctype raw data in bytes and get the properties
     skb_event = ct.cast(data, ct.POINTER(SkbEvent)).contents
-    print("[file size: {} B][executive size: {} B][media size: {} B]".format(skb_event.leaf.file_sz, skb_event.leaf.exec_sz, skb_event.leaf.media_sz))
-    # print (skb_event.magic)
+    raw_data = bytes(bytearray(skb_event.raw))
+    offset = int(skb_event.offset)  # payload offset
+    length = int(skb_event.real_len)  # true payload length
+    ip_src = int.from_bytes(raw_data[26:30], 'big')
+    ip_dst = int.from_bytes(raw_data[30:34], 'big')
+    port_src = int.from_bytes(raw_data[34:36], 'big')
+    port_dst = int.from_bytes(raw_data[36:38], 'big')
+    sig, fsz, csz, esz = 0, 0, 0, 0
+    suffix = None
+    current_key = sessions.Key(ip_src, ip_dst, port_src, port_dst)
+    # now we only consider one HTTP intact packet
+    if rest:
+        if length - offset < rest:
+            rest -= (length - offset)
+            return
+        else:
+            offset += rest
+            rest = 0
+    while offset < length:
+        # cope with the header
+        sig = int.from_bytes(raw_data[offset: offset + 4], 'little')
+        if sig != SIGNATURE_LOCAL_FILE_HEADER:
+            break
+        csz = int.from_bytes(raw_data[offset + 18: offset + 22], 'little')
+        fsz = int.from_bytes(raw_data[offset + 26: offset + 28], 'little')
+        esz = int.from_bytes(raw_data[offset + 28: offset + 30], 'little')
+        offset += 30
+
+        # cope with the file name
+        cont_sz += csz
+        # check if this file is document, the DOCX file is a zip file with a file named "[Content_Types].xml"
+        if not is_docx and raw_data[offset: offset + fsz] == b'[Content_Types].xml':
+            is_docx = True
+        if verbose:
+            print(raw_data[offset: offset + fsz].decode('utf-8'), "({} B)".format(csz))
+        if fsz >= 5:
+            suffix = raw_data[offset + fsz - 4: offset + fsz].decode('utf-8').lower()
+            if suffix == '.bin':
+                exec_sz += csz
+            if suffix == '.jpg' or suffix == '.png' or suffix == '.wmf' or suffix == '.svg' or suffix == 'jpeg':
+                media_sz += csz
+        offset += fsz
+
+        # cope with the last part
+        if length - offset < esz + csz:
+            rest = esz + csz - (length - offset)
+            break
+        offset += esz + csz
+
+    # cope with the whole file
+    if sig == SIGNATURE_CENTRAL_DIRECTORY:
+        if is_docx:  # only the document info will be printed
+            if verbose:
+                print("[content size: {} B][executive ratio: {}][media ratio: {}]".format(cont_sz, exec_sz / cont_sz,
+                                                                                          media_sz / cont_sz))
+            print("[{}.{}.{}.{}:{} -> {}.{}.{}.{}:{}]".format((ip_src >> 24 & 0xff), (ip_src >> 16 & 0xff),
+                                                              (ip_src >> 8 & 0xff), (ip_src & 0xff), port_src,
+                                                              (ip_dst >> 24 & 0xff), (ip_dst >> 16 & 0xff),
+                                                              (ip_dst >> 8 & 0xff), (ip_dst & 0xff), port_dst))
+            new_row = pd.DataFrame([[cont_sz, exec_sz, media_sz], ], columns=["fsize", "exec_ratio", "media_ratio"],
+                                   dtype=float)
+            print("[property: {}]".format("suspicious" if dt_model.predict(new_row) == [1] else "benign"))
+        # delete this item from the BPF MAP
+        if current_key in sessions:
+            del sessions[current_key]
+        # clear the variables and prepare the next file parsing
+        cont_sz, exec_sz, media_sz, rest, is_docx = 0, 0, 0, 0, False
 
 
 def clear_clsact():
@@ -58,6 +148,13 @@ def clear_clsact():
         os.system("tc qdisc del dev {} clsact".format(dev))
 
 
+auto_get_eth()
+dt_model = joblib.load('decision-tree.model')
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="Print the details of the document. This option will bring a lot of time "
+                                            "cost and is not recommended.", action="store_true")
+args = parser.parse_args()
+verbose = args.verbose
 try:
     b = BPF(src_file="tc_doc_log.c")
     fn = b.load_func("tc_check_docx", BPF.SCHED_CLS)
@@ -71,14 +168,13 @@ try:
            parent="ffff:fff2", classid=1, direct_action=True)
 
     b["skb_events"].open_perf_buffer(print_skb_event)
-    print("tc attach to dev [{}].".format(dev))
+    sessions = b['sessions']
+    print("scouter: tc attach to dev [{}].\n".format(dev))
     try:
         while True:
             b.perf_buffer_poll()
     except KeyboardInterrupt:
         pass
-    clear_clsact()
-    print("tc dettach from dev [{}].".format(dev))
 finally:
-    pass
-    # if "me" in locals(): ipr.link("del", index=me)
+    clear_clsact()
+    print("\nscouter: tc dettach from dev [{}].".format(dev))
